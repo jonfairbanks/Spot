e6b31b3ae5fd1c88b513db1fa328ce2dfd95a98f
Jon Fairbanks <fairplay89@gmail.com>
2019-08-04 17:59:50 -0700

    Code Clean up

--- spot-logger/controllers/spot.js
+++ spot-logger/controllers/spot.js
@@ -2,14 +2,9 @@
 let Spot = require('../models/spot');
 
 exports.get = (req, res) => {
-  let query = req.query || {};
-
-  Spot.apiQuery(query).find({}).sort({createdAt: '-1'}).limit(40)
-    .then(spots => {
-        res.json(spots);
-    })
-    .catch(err => {
-        res.status(422).send(err.errors);
-    });
-
+    let query = req.query || {};
+    
+    Spot.apiQuery(query).find({}).sort({createdAt: '-1'})
+    .then(spots => { res.json(spots) })
+    .catch(err => { res.status(422).send(err.errors) })
 };
