5edb5d91c44f6bbeaa69b0bffec052a5791bc8dd
Jon Fairbanks <fairplay89@gmail.com>
2019-08-07 21:37:39 -0700

    Add Action Button

--- spot-calculator/screens/PortfolioScreen.js
+++ spot-calculator/screens/PortfolioScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ActivityIndicator, Text, View, StyleSheet, AsyncStorage } from 'react-native';
 import { MonoText } from '../components/StyledText';
+import ActionButton from 'react-native-action-button';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class PortfolioScreen extends React.Component {
   constructor(props){
@@ -53,6 +55,17 @@ export default class PortfolioScreen extends React.Component {
             ${this.state.dataSource * 1} (Based on 1 oz)
           </MonoText>
         </View>
+        <ActionButton buttonColor="rgba(231,76,60,1)">
+          <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
+            <Icon name="md-create" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item buttonColor='#3498db' title="Notifications" onPress={() => {}}>
+            <Icon name="md-notifications-off" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item buttonColor='#1abc9c' title="All Tasks" onPress={() => {}}>
+            <Icon name="md-done-all" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+        </ActionButton>
       </View>
     );
   }
@@ -78,5 +91,10 @@ const styles = StyleSheet.create({
   },
   navigationFilename: {
     marginTop: 5,
+  },
+  actionButtonIcon: {
+    fontSize: 20,
+    height: 22,
+    color: 'white',
   },
 });
